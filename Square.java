a7d65b177e7b040fe57389cea179028e1b19b089
Kirill20704 <73089058+Kirill20704@users.noreply.github.com>
2022-10-13 19:51:00 +0700

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,10 @@
+package com.figr;
+public class Square extends Rectangle{
+
+    public Square(double side) {
+        super(side,side);
+    }
+    public void setSide(double side) {
+        super.setSide(side, side);
+    }
+}
